139ba09f1f1351b186d6b665a300e80e03f65881
VarunV-007 <97v.varun@gmail.com>
2019-04-08 17:20:36 +0530

    updated extension.ts

--- out/extension.js
+++ out/extension.js
@@ -1,130 +1,155 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
 const child_process = require("child_process");
-let isDictation = false;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10);
-    statusBarItem.text = '💬 Listen';
-    statusBarItem.command = 'extension.listen';
-    statusBarItem.show();
-    let recognizer;
-    let suggest_tree = new SampleTree();
-    let dictate_tree = new SampleTree();
-    let errors_tree = new SampleTree();
-    vscode.window.createTreeView('suggestions', { treeDataProvider: suggest_tree });
-    vscode.window.createTreeView('dictation', { treeDataProvider: dictate_tree });
-    vscode.window.createTreeView('errors', { treeDataProvider: errors_tree });
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable1 = vscode.commands.registerCommand('extension.listen', () => {
-        // The code you place here will be executed every time your command is executed
-        statusBarItem.text = '✖️️ Stop';
-        statusBarItem.command = 'extension.stop';
-        statusBarItem.show();
-        let ed = new Editor();
-        recognizer = child_process.spawn('python', [`${__dirname}/audio.py`]);
-        recognizer.stdout.on('data', function (data) {
-            let JSONdata = JSON.parse(data);
-            console.log(JSONdata);
-            if (JSONdata.hasOwnProperty('status') && JSONdata.status === 'ready') {
-                vscode.window.showInformationMessage(`${JSONdata.status}`);
-            }
-            else if (JSONdata.hasOwnProperty('error')) {
-                let tree = [new SampleNode(JSONdata.input, []), new SampleNode(JSONdata.error, [])];
-                suggest_tree.setTree(tree);
-            }
-            else {
-                let tree = [new SampleNode(JSONdata.input, [])];
-                for (let i = 1; i <= JSONdata.output.length; ++i) {
-                    tree.push(new SampleNode(`${i}. ${JSONdata.output[i - 1].code} (${JSONdata.output[i - 1].construct})`, []));
-                }
-                suggest_tree.setTree(tree);
-                ed.insertText(JSONdata.output[0].code + '\n');
-                if (JSONdata.output[0].construct === 'function') {
-                    const position = ed.getCursorPosition();
-                    console.log(position);
-                    let newPosition = position.with(position.line + 1, 0);
-                    ed.moveCursor(newPosition);
-                }
-            }
-        });
-        recognizer.stderr.on('data', function (data) {
-            console.log(`error: ${data}`);
-        });
-        recognizer.on('close', function (code) {
-            console.log('program stopped');
-            statusBarItem.text = '💬 Listen';
-            statusBarItem.command = 'extension.listen';
-            statusBarItem.show();
-        });
+    let codac = new Codac();
+    let listen = vscode.commands.registerCommand('extension.listen', () => {
+        codac.startRecognizer('audio');
     });
-    let disposable2 = vscode.commands.registerCommand('extension.stop', () => {
-        console.log('killing program');
-        recognizer.kill();
-        statusBarItem.text = '💬 Listen';
-        statusBarItem.command = 'extension.listen';
-        statusBarItem.show();
-        isDictation = false;
+    let stop = vscode.commands.registerCommand('extension.stop', () => {
+        codac.stopRecognizer();
     });
-    let disposable3 = vscode.commands.registerCommand('extension.dictation', () => {
-        isDictation = (isDictation) ? false : true;
-        console.log(isDictation);
-        if (isDictation)
-        {
-            statusBarItem.text = '✖️️ Stop Dictation';
-            statusBarItem.command = 'extension.stop';
-            statusBarItem.show();
-            recognizer = child_process.spawn('python', [`${__dirname}/dictate_audio.py`]);
-            recognizer.stdout.on('data', function (data) {
-                let JSONdata = JSON.parse(data);
-                console.log(JSONdata);
-                if (JSONdata.hasOwnProperty('status') && JSONdata.status === 'ready') {
-                    vscode.window.showInformationMessage(`${JSONdata.status}`);
-                }
-                else if (JSONdata.hasOwnProperty('error')) {
-                    let tree = [new SampleNode(JSONdata.input, []), new SampleNode(JSONdata.error, [])];
-                    dictate_tree.setTree(tree);
-                }
-                else {
-                    let tree = [new SampleNode('Your string :', [])];
-                    for (let i = 1; i <= JSONdata.output.length; ++i) {
-                        tree.push(new SampleNode(`${i}. ${JSONdata.output[i - 1]}`, []));
-                    }
-                    dictate_tree.setTree(tree);
-                    // append the tree and send to editor
-                }
-            });
-            recognizer.stderr.on('data', function (data) {
-                console.log(`error: ${data}`);
-            });
-            recognizer.on('close',  function (code) {
-                console.log('program stopped');
-                statusBarItem.text = '💬 Listen';
-                statusBarItem.command = 'extension.listen';
-                statusBarItem.show();
-            });
-        }
-        else{
-            vscode.commands.executeCommand('extension.stop');
-        }
+    let dictation = vscode.commands.registerCommand('extension.dictation', () => {
+        codac.dictate();
     });
-
-    context.subscriptions.push(disposable1);
-    context.subscriptions.push(disposable2);
-    context.subscriptions.push(disposable3);
+    context.subscriptions.push(listen);
+    context.subscriptions.push(stop);
+    context.subscriptions.push(dictation);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
+class Codac {
+    constructor() {
+        this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10);
+        this.setStatusBarBtn('💬 Listen', 'extension.listen');
+        this.suggestTree = new SampleTree();
+        this.dictateTree = new SampleTree();
+        this.errorTree = new SampleTree();
+        vscode.window.createTreeView('suggestions', { treeDataProvider: this.suggestTree });
+        vscode.window.createTreeView('dictation', { treeDataProvider: this.dictateTree });
+        vscode.window.createTreeView('errors', { treeDataProvider: this.errorTree });
+        this.editor = new Editor();
+        this.recognizer = Object();
+        this.isDictation = false;
+        this.isListening = false;
+    }
+    setStatusBarBtn(text, command) {
+        this.statusBarItem.text = text;
+        this.statusBarItem.command = command;
+        this.statusBarItem.show();
+    }
+    handleOutput(tree, data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let self = this;
+            console.log(data.toString(), data.toString().length);
+            let JSONdata = JSON.parse(data.toString());
+            if (JSONdata.hasOwnProperty('status') && JSONdata.status === 'ready') {
+                vscode.window.showInformationMessage(`${JSONdata.status}`);
+            }
+            else if (JSONdata.hasOwnProperty('error')) {
+                let treeData = [new SampleNode(JSONdata.error, [])];
+                tree.setTree(treeData);
+            }
+            else {
+                let treeData = [new SampleNode(JSONdata[0].input, [])];
+                for (let i = 1; i < JSONdata.length; ++i) {
+                    treeData.push(new SampleNode(`${i}. ${JSONdata[i].output}`, []));
+                }
+                tree.setTree(treeData);
+                yield self.replaceCode(JSONdata[1]['replace'], JSONdata[1]['cursor']);
+            }
+        });
+    }
+    replaceCode(statement, cursor) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let self = this;
+            yield vscode.commands.executeCommand('editor.action.selectAll');
+            yield vscode.commands.executeCommand('editor.action.deleteLines');
+            yield self.editor.insertText(statement);
+            yield self.editor.moveCursor(new vscode.Position(cursor - 1, 0));
+            yield vscode.commands.executeCommand('cursorEnd');
+        });
+    }
+    startRecognizer(choice) {
+        let self = this;
+        let options;
+        let tree;
+        options = {
+            'audio': [
+                `${__dirname}/audio.py`,
+                `${self.editor.getFileName()}`,
+                `${self.editor.getCursorPosition().line + 1}`
+            ],
+            'dictate_audio': [
+                'dictate_audio.py'
+            ]
+        };
+        if (choice === 'audio') {
+            this.isListening = true;
+            tree = this.suggestTree;
+        }
+        else {
+            tree = this.dictateTree;
+        }
+        self.recognizer = child_process.spawn('python', options[choice]);
+        self.recognizer.stdout.on('data', function (data) {
+            try {
+                self.handleOutput(tree, data);
+            }
+            catch (e) {
+                console.log(e);
+            }
+        });
+        self.recognizer.on('close', function (code, signal) {
+            console.log('program stopped', signal);
+            if (signal === null) {
+                console.log('respawning');
+                self.setStatusBarBtn('✖️️ Stop', 'extension.stop');
+                self.startRecognizer('audio');
+            }
+            else {
+                self.setStatusBarBtn('💬 Listen', 'extension.listen');
+            }
+        });
+        self.recognizer.stderr.on('data', function (data) {
+            console.log(`error: ${data}`);
+        });
+        self.setStatusBarBtn('✖️️ Stop', 'extension.stop');
+    }
+    stopRecognizer() {
+        this.recognizer.kill('SIGKILL');
+        this.setStatusBarBtn('💬 Listen', 'extension.listen');
+        this.isDictation = this.isListening = false;
+    }
+    dictate() {
+        this.isDictation = (this.isDictation) ? false : true;
+        if (this.isDictation) {
+            if (this.isListening) {
+                this.stopRecognizer();
+            }
+            this.startRecognizer('dictate_audio');
+            this.setStatusBarBtn('✖️️ Stop Dictation', 'extension.stop');
+        }
+        else {
+            this.stopRecognizer();
+        }
+    }
+}
 class SampleNode extends vscode.TreeItem {
     constructor(label, children) {
         super(label);
@@ -150,12 +175,8 @@ class SampleTree {
         return element ? element.getChildren() : this.tree;
     }
     setTree(tree) {
+        console.log(tree);
         this.tree = tree;
-        this._onDidChangeTreeData.fire();
-    }
-    updateTree(tree) {
-        console.log(this.tree)
-        this.tree = this.tree + tree;
         this._onDidChangeTreeData.fire();
     }
 }
@@ -169,7 +190,7 @@ class Editor {
             if (editor.selection.isEmpty) {
                 pos = editor.selection.active;
             }
-            console.log(pos.line, pos.character);
+            console.log(pos.line, pos.character, text);
             editor.edit(editBuilder => {
                 editBuilder.insert(pos, text);
             });
@@ -189,6 +210,13 @@ class Editor {
             return position;
         }
         return new vscode.Position(0, 0);
+    }
+    getFileName() {
+        if (vscode.window.activeTextEditor) {
+            const editor = vscode.window.activeTextEditor;
+            return editor.document.uri.fsPath;
+        }
+        return '';
     }
 }
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -6,83 +6,159 @@ import * as child_process from 'child_process';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let codac = new Codac();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	let statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10);
-	statusBarItem.text = '💬 Listen';
-	statusBarItem.command = 'extension.listen';
-	statusBarItem.show();
-
-	let recognizer: any;
-
-	let sampleTree = new SampleTree();
-
-	vscode.window.createTreeView('suggestions', {treeDataProvider: sampleTree});
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable1 = vscode.commands.registerCommand('extension.listen', () => {
-		// The code you place here will be executed every time your command is executed
-
-		statusBarItem.text = '✖️️ Stop';
-		statusBarItem.command = 'extension.stop';
-		statusBarItem.show();
-
-		let ed = new Editor();
-		recognizer = child_process.spawn('python', [`${__dirname}/audio.py`]);
-		
-		recognizer.stdout.on('data', function(data: string) {
-			let JSONdata = JSON.parse(data);
-			console.log(JSONdata);
-			if (JSONdata.hasOwnProperty('status') && JSONdata.status === 'ready') {
-				vscode.window.showInformationMessage(`${JSONdata.status}`);
-			} else if (JSONdata.hasOwnProperty('error')) {
-				let tree = [new SampleNode(JSONdata.input, []), new SampleNode(JSONdata.error, [])];
-				sampleTree.setTree(tree);
-			} else {
-				let tree = [new SampleNode(JSONdata.input, [])];
-				for (let i = 1; i <= JSONdata.output.length; ++i) {
-					tree.push(new SampleNode(`${i}. ${JSONdata.output[i - 1].code} (${JSONdata.output[i - 1].construct})`, []));
-				}
-				sampleTree.setTree(tree);
-				ed.insertText(JSONdata.output[0].code + '\n');
-				if (JSONdata.output[0].construct === 'function') {
-					const position = ed.getCursorPosition();
-					console.log(position);
-					let newPosition = position.with(position.line + 1, 0);
-					ed.moveCursor(newPosition);
-				}
-			}
-		});
-
-		recognizer.stderr.on('data', function(data: string) {
-			console.log(`error: ${data}`);
-		});
-
-		recognizer.on('close', function(code: string) {
-			console.log('program stopped');
-			statusBarItem.text = '💬 Listen';
-			statusBarItem.command = 'extension.listen';
-			statusBarItem.show();
-		});
+	let listen = vscode.commands.registerCommand('extension.listen', () => {
+		codac.startRecognizer('audio');
+	});
+	let stop = vscode.commands.registerCommand('extension.stop', () => {
+		codac.stopRecognizer();
+	});
+	let dictation = vscode.commands.registerCommand('extension.dictation', () => {
+		codac.dictate();
 	});
 
-	let disposable2 = vscode.commands.registerCommand('extension.stop', () => {
-		console.log('killing program');
-		recognizer.kill();
-		statusBarItem.text = '💬 Listen';
-		statusBarItem.command = 'extension.listen';
-		statusBarItem.show();
-	});
-
-	context.subscriptions.push(disposable1);
-	context.subscriptions.push(disposable2);
+	context.subscriptions.push(listen);
+	context.subscriptions.push(stop);
+	context.subscriptions.push(dictation);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+class Codac {
+	private statusBarItem: vscode.StatusBarItem;
+	private suggestTree: SampleTree; 
+	private dictateTree: SampleTree;
+	private errorTree: SampleTree;
+	private editor: Editor;
+	private recognizer: child_process.ChildProcess;
+	private isDictation: boolean;
+	private isListening: boolean;
+
+	constructor() {
+		this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 10);
+		this.setStatusBarBtn('💬 Listen', 'extension.listen');
+
+		this.suggestTree = new SampleTree();
+		this.dictateTree = new SampleTree();
+		this.errorTree = new SampleTree();
+		vscode.window.createTreeView('suggestions', {treeDataProvider: this.suggestTree});
+		vscode.window.createTreeView('dictation', {treeDataProvider: this.dictateTree});
+		vscode.window.createTreeView('errors', {treeDataProvider: this.errorTree});
+
+		this.editor = new Editor();
+		this.recognizer = Object();
+
+		this.isDictation = false;
+		this.isListening = false;
+	}
+
+	public setStatusBarBtn(text: string, command: string) {
+		this.statusBarItem.text = text;
+		this.statusBarItem.command = command;
+		this.statusBarItem.show();
+	}
+
+	public async handleOutput(tree: SampleTree, data: any) {
+		let self = this;
+		console.log(data.toString(), data.toString().length);
+		let JSONdata = JSON.parse(data.toString());
+		if (JSONdata.hasOwnProperty('status') && JSONdata.status === 'ready') {
+			vscode.window.showInformationMessage(`${JSONdata.status}`);
+		} else if (JSONdata.hasOwnProperty('error')) {
+			let treeData = [new SampleNode(JSONdata.error, [])];
+			tree.setTree(treeData);
+		} else {
+			let treeData = [new SampleNode(JSONdata[0].input, [])];
+			for (let i = 1; i < JSONdata.length; ++i) {
+				treeData.push(new SampleNode(`${i}. ${JSONdata[i].output}`, []));
+			}
+			tree.setTree(treeData);
+			await self.replaceCode(JSONdata[1]['replace'], JSONdata[1]['cursor']);
+		}
+	}
+
+	public async replaceCode(statement: string, cursor: number) {
+		let self = this;		
+		await vscode.commands.executeCommand('editor.action.selectAll');
+		await vscode.commands.executeCommand('editor.action.deleteLines');
+		await self.editor.insertText(statement);
+		await self.editor.moveCursor(new vscode.Position(cursor - 1, 0));
+		await vscode.commands.executeCommand('cursorEnd');
+	}
+
+	public startRecognizer(choice: string) {
+		let self = this;
+		let options: any;
+		let tree: SampleTree;
+		options = {
+			'audio': [
+				`${__dirname}/audio.py`,
+			 	`${self.editor.getFileName()}`,
+			 	`${self.editor.getCursorPosition().line + 1}`
+			],
+			'dictate_audio': [
+				'dictate_audio.py'
+			]
+		};
+		if (choice === 'audio') {
+			this.isListening = true;
+			tree = this.suggestTree;
+		} else {
+			tree = this.dictateTree;
+		}
+
+		self.recognizer = child_process.spawn(
+			'python',
+			options[choice]
+		);
+	
+		self.recognizer.stdout.on('data', function(data: string) {
+			try {
+				self.handleOutput(tree, data);
+			} catch(e) {
+				console.log(e);
+			}
+		});
+	
+		self.recognizer.on('close', function(code: number, signal: string) {
+			console.log('program stopped', signal);
+			if (signal === null) {
+				console.log('respawning');
+				self.setStatusBarBtn('✖️️ Stop', 'extension.stop');
+				self.startRecognizer('audio');
+			} else {
+				self.setStatusBarBtn('💬 Listen', 'extension.listen');
+			}
+		});
+	
+		self.recognizer.stderr.on('data', function(data: string) {
+			console.log(`error: ${data}`);
+		});
+
+		self.setStatusBarBtn('✖️️ Stop', 'extension.stop');
+	}
+
+	public stopRecognizer() {
+		this.recognizer.kill('SIGKILL');
+		this.setStatusBarBtn('💬 Listen', 'extension.listen');
+		this.isDictation = this.isListening = false;
+	}
+
+	public dictate() {
+		this.isDictation = (this.isDictation) ? false : true;
+		if (this.isDictation) {
+			if (this.isListening) {
+				this.stopRecognizer();
+			}
+			this.startRecognizer('dictate_audio');
+			this.setStatusBarBtn('✖️️ Stop Dictation', 'extension.stop');
+		} else {
+			this.stopRecognizer();			
+		}
+	}
+}
 
 class SampleNode extends vscode.TreeItem{
 	private children: SampleNode[];
@@ -116,6 +192,7 @@ class SampleTree implements vscode.TreeDataProvider<vscode.TreeItem> {
 	}
 
 	public setTree(tree: SampleNode[]) {
+		console.log(tree);
 		this.tree = tree;
 		this._onDidChangeTreeData.fire();
 	}
@@ -133,7 +210,7 @@ class Editor {
 			if (editor.selection.isEmpty) {
 				pos = editor.selection.active;
 			}
-			console.log(pos.line, pos.character);
+			console.log(pos.line, pos.character, text);
 			editor.edit(
 				editBuilder => {
 					editBuilder.insert(pos, text);
@@ -145,7 +222,6 @@ class Editor {
 	public moveCursor(position: vscode.Position) {
 		if (vscode.window.activeTextEditor !== undefined) {
 			const editor = vscode.window.activeTextEditor;
-
 			var newSelection = new vscode.Selection(position, position);
 			editor.selection = newSelection;
 		}
@@ -155,9 +231,16 @@ class Editor {
 		if (vscode.window.activeTextEditor) {
 			const editor = vscode.window.activeTextEditor;
 			const position = editor.selection.active;
-
 			return position;
 		}
 		return new vscode.Position(0, 0);
+	}
+
+	public getFileName(): string {
+		if (vscode.window.activeTextEditor) {
+			const editor = vscode.window.activeTextEditor;
+			return editor.document.uri.fsPath;
+		}
+		return '';
 	}
 }
